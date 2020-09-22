b8e7663457b2ed6be3033cb8443ad1541d4597c6
mosguinz <mos.guinz@gmail.com>
2020-09-22 13:50:21 -0700

    Initial commit

---
+++ src/BmiMethods.java
@@ -0,0 +1,33 @@
+/*
+ * Assignment 04
+ * Description: [Part 1 of 2] BMI using methods
+ * Name: Kullathon "Mos" Sitthisarnwattanachai
+ * Teammate: Amber Hartigan
+ * ID: 921425216
+ * Class: CSC 210-03
+ * Semester: Fall 2020
+ */
+
+public class BmiMethods {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Contains execution loop for the standard BMI calculator.
+     * <p>
+     * See also: https://github.com/mosguinz-csc210-03/CSC210Asmt01/blob/master/src/BMI.java
+     */
+    public static void bmiStd() {
+
+    }
+
+    /**
+     * Contains execution loop for the BMI table calculator.
+     * <p>
+     * See also: https://github.com/mosguinz-csc210-03/CSC210Asmt03/blob/master/src/TableBmi.java
+     */
+    public static void bmiPro() {
+
+    }
+}
