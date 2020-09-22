9182ced9fde1eb63ec9b9ee091940a1f96789783
mosguinz <mos.guinz@gmail.com>
2020-09-22 14:20:53 -0700

    Add dice class

---
+++ src/Dice.java
@@ -0,0 +1,15 @@
+/*
+ * Assignment 04
+ * Description: [Part 2 of 2] Dice value tracker
+ * Name: Kullathon "Mos" Sitthisarnwattanachai
+ * Teammate: Amber Hartigan
+ * ID: 921425216
+ * Class: CSC 210-03
+ * Semester: Fall 2020
+ */
+
+public class Dice {
+    public static void main(String[] args) {
+
+    }
+}
