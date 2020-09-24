60a4fb5c60ccbcd1a4d1c995deb2efc4459bb1df
mosguinz <mos.guinz@gmail.com>
2020-09-23 17:42:48 -0700

    Add method to prompt number of throws

--- src/Dice.java
+++ src/Dice.java
@@ -8,8 +8,39 @@
  * Semester: Fall 2020
  */
 
-public class Dice {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Dice {
+
+    private static final int DICE_FACES = 6;
+    private static final int MIN_THROWS = 1;
+    private static final int MAX_THROWS = 1000000;
+
+    public static void main(String[] args) {
+        loop();
+    }
+
+    public static void loop() {
+        Scanner scan = new Scanner(System.in);
+        int nThrows = promptThrows(scan);
+
+    }
+
+    /**
+     * Prompt the user to enter number of throws they would like to perform. The
+     * prompt will continue until the user enters a value within the valid
+     * bounds (MIN_THROWS <= t <= MAX_THROWS).
+     *
+     * @param scan The `Scanner` object to use.
+     * @return The valid number of throws.
+     */
+    private static int promptThrows(Scanner scan) {
+        int t;
+        do {
+            System.out.printf("Enter the number of throws between %,d to %,d inclusive.%n", MIN_THROWS, MAX_THROWS);
+            System.out.print("Or enter 0 to quit: ");
+            t = scan.nextInt();
+        } while (t < MIN_THROWS || t > MAX_THROWS);
+        return t;
     }
 }
