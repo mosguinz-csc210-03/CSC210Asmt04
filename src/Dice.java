f9477296cf75dc1e18c34dffb10951d50ff1b64b
mosguinz <mos.guinz@gmail.com>
2020-09-23 18:02:20 -0700

    Rename dice roll references from "throw" to "roll"

--- src/Dice.java
+++ src/Dice.java
@@ -14,8 +14,8 @@ import java.util.Scanner;
 public class Dice {
 
     private static final int DICE_FACES = 6;
-    private static final int MIN_THROWS = 1;
-    private static final int MAX_THROWS = 1000000;
+    private static final int MIN_ROLLS = 1;
+    private static final int MAX_ROLLS = 1000000;
 
     public static void main(String[] args) {
         loop();
@@ -23,25 +23,25 @@ public class Dice {
 
     public static void loop() {
         Scanner scan = new Scanner(System.in);
-        int nThrows = promptThrows(scan);
+        int nThrows = promptRolls(scan);
 
     }
 
     /**
      * Prompt the user to enter number of throws they would like to perform. The
      * prompt will continue until the user enters a value within the valid
-     * bounds (MIN_THROWS <= t <= MAX_THROWS).
+     * bounds (MIN_ROLLS <= t <= MAX_ROLLS).
      *
      * @param scan The `Scanner` object to use.
      * @return The valid number of throws.
      */
-    private static int promptThrows(Scanner scan) {
+    private static int promptRolls(Scanner scan) {
         int t;
         do {
-            System.out.printf("Enter the number of throws between %,d to %,d inclusive.%n", MIN_THROWS, MAX_THROWS);
+            System.out.printf("Enter the number of throws between %,d to %,d inclusive.%n", MIN_ROLLS, MAX_ROLLS);
             System.out.print("Or enter 0 to quit: ");
             t = scan.nextInt();
-        } while (t < MIN_THROWS || t > MAX_THROWS);
+        } while (t < MIN_ROLLS || t > MAX_ROLLS);
         return t;
     }
 
@@ -53,5 +53,9 @@ public class Dice {
      */
     private static int rollDice() {
         return new Random().nextInt(DICE_FACES) + 1;
+    }
+
+    private static void displayRollFreq() {
+
     }
 }
